ab2b22d95ae7026b40efd24ba78ed53afc5b8ccc
Gérald Lelong <gerald.lelong@easymov.fr>
2019-12-03 11:24:25 +0100

    day2 part2

--- src/day2.rs
+++ src/day2.rs
@@ -1,21 +1,24 @@
 use anyhow::{anyhow, bail, Result};
+use itertools::Itertools;
 use std::fs;
 
 fn main() -> Result<()> {
     let input = fs::read_to_string("inputs/day2.txt")?;
-    let mut intcode: Vec<usize> = input
+    let intcode: Vec<usize> = input
         .split(',')
         .map(|opcode| opcode.parse().unwrap())
         .collect();
-    intcode[1] = 12;
-    intcode[2] = 2;
-    let result = process_intcode(&intcode)?;
+    let result = process_intcode(&intcode, 12, 2)?;
     println!("{}", result[0]);
+    let result = bruteforce(&intcode, 19_690_720).unwrap();
+    println!("{}", result.0 * 100 + result.1);
     Ok(())
 }
 
-fn process_intcode(intcode: &[usize]) -> Result<Vec<usize>> {
+fn process_intcode(intcode: &[usize], noun: usize, verb: usize) -> Result<Vec<usize>> {
     let mut intcode = intcode.to_vec();
+    intcode[1] = noun;
+    intcode[2] = verb;
     let mut ip = 0;
     while let Some(opcode) = intcode.get(ip) {
         match opcode {
@@ -40,25 +43,37 @@ fn process_intcode(intcode: &[usize]) -> Result<Vec<usize>> {
     Err(anyhow!("no end found"))
 }
 
+fn bruteforce(intcode: &[usize], output: usize) -> Option<(usize, usize)> {
+    (0..99)
+        .cartesian_product(0..99)
+        .find(|(noun, verb)| process_intcode(intcode, *noun, *verb).unwrap()[0] == output)
+}
+
 #[test]
 fn test() {
     assert_eq!(
-        *process_intcode(&[1, 0, 0, 0, 99]).unwrap().first().unwrap(),
-        2
-    );
-    assert_eq!(
-        *process_intcode(&[2, 3, 0, 3, 99]).unwrap().first().unwrap(),
-        2
-    );
-    assert_eq!(
-        *process_intcode(&[2, 4, 4, 5, 99, 0])
+        *process_intcode(&[1, 0, 0, 0, 99], 0, 0)
             .unwrap()
             .first()
             .unwrap(),
         2
     );
     assert_eq!(
-        *process_intcode(&[1, 1, 1, 4, 99, 5, 6, 0, 99])
+        *process_intcode(&[2, 3, 0, 3, 99], 3, 0)
+            .unwrap()
+            .first()
+            .unwrap(),
+        2
+    );
+    assert_eq!(
+        *process_intcode(&[2, 4, 4, 5, 99, 0], 4, 4)
+            .unwrap()
+            .first()
+            .unwrap(),
+        2
+    );
+    assert_eq!(
+        *process_intcode(&[1, 1, 1, 4, 99, 5, 6, 0, 99], 1, 1)
             .unwrap()
             .first()
             .unwrap(),
