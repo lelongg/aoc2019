b40fb0ee935c94c3d511f48ea901f99c9673fb8f
Gérald Lelong <gerald.lelong@easymov.fr>
2019-12-07 19:09:48 +0100

    day5 part2

--- src/day5.rs
+++ src/day5.rs
@@ -1,7 +1,16 @@
 use anyhow::{anyhow, bail, Result};
-use itertools::Itertools;
 use std::fs;
 use std::io::Write;
+
+fn read_from_stdin() -> Option<i32> {
+    let mut input = String::new();
+    print!("input: ");
+    std::io::stdout().flush().unwrap();
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("failed to read from stdin");
+    Some(input.trim().parse().expect("not a number"))
+}
 
 fn main() -> Result<()> {
     let input = fs::read_to_string("inputs/day5.txt")?;
@@ -10,7 +19,13 @@ fn main() -> Result<()> {
         .split(',')
         .map(|opcode| opcode.parse().unwrap())
         .collect();
-    let _result = process_intcode(&intcode, None, None)?;
+    let mut output = Vec::new();
+    process_intcode(
+        &intcode,
+        &mut std::iter::from_fn(read_from_stdin),
+        &mut output,
+    )?;
+    println!("{:?}", output);
     Ok(())
 }
 
@@ -47,9 +62,26 @@ impl ParameterMode {
     }
 }
 
+#[derive(Clone, Debug, PartialEq)]
+struct Parameters(Vec<ParameterMode>);
+
+impl Parameters {
+    fn get(&self, index: usize, intcode: &[i32], ip: usize) -> i32 {
+        self.0
+            .get(index)
+            .copied()
+            .unwrap_or_default()
+            .get(intcode, ip + index + 1)
+    }
+
+    fn get_mut<'a>(&self, index: usize, intcode: &'a mut [i32], ip: usize) -> &'a mut i32 {
+        ParameterMode::Position.get_mut(intcode, ip + index + 1)
+    }
+}
+
 struct Opcode {
     code: usize,
-    parameters_mode: Vec<ParameterMode>,
+    parameters: Parameters,
 }
 
 impl Opcode {
@@ -63,141 +95,229 @@ impl Opcode {
             .collect::<String>()
             .parse()
             .unwrap();
-        let parameters_mode = digits
-            .iter()
-            .map(|digit| digit.to_digit(10).unwrap())
-            .map(ParameterMode::new)
-            .collect();
-        Opcode {
-            code,
-            parameters_mode,
-        }
+        let parameters = Parameters(
+            digits
+                .iter()
+                .map(|digit| digit.to_digit(10).unwrap())
+                .map(ParameterMode::new)
+                .collect(),
+        );
+        Opcode { code, parameters }
     }
 }
 
-struct Add;
-impl Add {
-    fn execute(parameters_mode: &[ParameterMode], intcode: &mut [i32], ip: &mut usize) {
-        let operand1 = parameters_mode
-            .get(0)
-            .copied()
-            .unwrap_or_default()
-            .get(intcode as &[i32], *ip + 1);
-        let operand2 = parameters_mode
-            .get(1)
-            .copied()
-            .unwrap_or_default()
-            .get(intcode as &[i32], *ip + 2);
-        let output = ParameterMode::Position.get_mut(intcode, *ip + 3);
-        *output = operand1 + operand2;
-        *ip += 4;
+fn add(parameters: &Parameters, intcode: &mut [i32], ip: &mut usize) {
+    let operand1 = parameters.get(0, intcode, *ip);
+    let operand2 = parameters.get(1, intcode, *ip);
+    let output = parameters.get_mut(2, intcode, *ip);
+    *output = operand1 + operand2;
+    *ip += 4;
+}
+
+fn mul(parameters: &Parameters, intcode: &mut [i32], ip: &mut usize) {
+    let operand1 = parameters.get(0, intcode, *ip);
+    let operand2 = parameters.get(1, intcode, *ip);
+    let output = parameters.get_mut(2, intcode, *ip);
+    *output = operand1 * operand2;
+    *ip += 4;
+}
+
+fn input(
+    stdin: &mut impl Iterator<Item = i32>,
+    parameters: &Parameters,
+    intcode: &mut [i32],
+    ip: &mut usize,
+) {
+    let output = parameters.get_mut(0, intcode, *ip);
+    let input = stdin.next().expect("no input");
+    *output = input;
+    *ip += 2;
+}
+
+fn output(stdout: &mut Vec<i32>, parameters: &Parameters, intcode: &mut [i32], ip: &mut usize) {
+    let operand1 = parameters.get(0, intcode, *ip);
+    stdout.push(operand1);
+    *ip += 2;
+}
+
+fn jump_if(parameters: &Parameters, intcode: &mut [i32], ip: &mut usize) {
+    let condition = parameters.get(0, intcode, *ip);
+    let jump_addr = parameters.get(1, intcode, *ip);
+    if condition != 0 {
+        *ip = jump_addr as usize;
+    } else {
+        *ip += 3;
     }
 }
 
-struct Mul;
-impl Mul {
-    fn execute(parameters_mode: &[ParameterMode], intcode: &mut [i32], ip: &mut usize) {
-        let operand1 = parameters_mode
-            .get(0)
-            .copied()
-            .unwrap_or_default()
-            .get(intcode as &[i32], *ip + 1);
-        let operand2 = parameters_mode
-            .get(1)
-            .copied()
-            .unwrap_or_default()
-            .get(intcode as &[i32], *ip + 2);
-        let output = ParameterMode::Position.get_mut(intcode, *ip + 3);
-        *output = operand1 * operand2;
-        *ip += 4;
+fn jump_unless(parameters: &Parameters, intcode: &mut [i32], ip: &mut usize) {
+    let condition = parameters.get(0, intcode, *ip);
+    let jump_addr = parameters.get(1, intcode, *ip);
+    if condition == 0 {
+        *ip = jump_addr as usize;
+    } else {
+        *ip += 3;
     }
 }
 
-struct In;
-impl In {
-    fn execute(_: &[ParameterMode], intcode: &mut [i32], ip: &mut usize) {
-        let output = ParameterMode::Position.get_mut(intcode, *ip + 1);
-        let mut input = String::new();
-        print!("input: ");
-        std::io::stdout().flush().unwrap();
-        std::io::stdin()
-            .read_line(&mut input)
-            .expect("failed to read from stdin");
-        *output = input.trim().parse().expect("not a number");
-        *ip += 2;
-    }
+fn is_less_than(parameters: &Parameters, intcode: &mut [i32], ip: &mut usize) {
+    let operand1 = parameters.get(0, intcode, *ip);
+    let operand2 = parameters.get(1, intcode, *ip);
+    let output = parameters.get_mut(2, intcode, *ip);
+    *output = if operand1 < operand2 { 1 } else { 0 };
+    *ip += 4;
 }
 
-struct Out;
-impl Out {
-    fn execute(parameters_mode: &[ParameterMode], intcode: &mut [i32], ip: &mut usize) {
-        let operand1 = parameters_mode
-            .get(0)
-            .copied()
-            .unwrap_or_default()
-            .get(intcode as &[i32], *ip + 1);
-        println!("output: {}", operand1);
-        *ip += 2;
-    }
+fn is_equal(parameters: &Parameters, intcode: &mut [i32], ip: &mut usize) {
+    let operand1 = parameters.get(0, intcode, *ip);
+    let operand2 = parameters.get(1, intcode, *ip);
+    let output = parameters.get_mut(2, intcode, *ip);
+    *output = if operand1 == operand2 { 1 } else { 0 };
+    *ip += 4;
 }
 
-fn process_intcode(intcode: &[i32], noun: Option<i32>, verb: Option<i32>) -> Result<Vec<i32>> {
+fn process_intcode(
+    intcode: &[i32],
+    stdin: &mut impl Iterator<Item = i32>,
+    stdout: &mut Vec<i32>,
+) -> Result<()> {
     let mut intcode = intcode.to_vec();
-    if let Some(noun) = noun {
-        intcode[1] = noun;
-    }
-    if let Some(verb) = verb {
-        intcode[2] = verb;
-    }
     let mut ip = 0;
     while let Some(opcode) = intcode.get(ip).copied().map(Opcode::new) {
         match opcode.code {
-            1 => Add::execute(&opcode.parameters_mode, &mut intcode, &mut ip),
-            2 => Mul::execute(&opcode.parameters_mode, &mut intcode, &mut ip),
-            3 => In::execute(&opcode.parameters_mode, &mut intcode, &mut ip),
-            4 => Out::execute(&opcode.parameters_mode, &mut intcode, &mut ip),
-            99 => return Ok(intcode),
+            1 => add(&opcode.parameters, &mut intcode, &mut ip),
+            2 => mul(&opcode.parameters, &mut intcode, &mut ip),
+            3 => input(stdin, &opcode.parameters, &mut intcode, &mut ip),
+            4 => output(stdout, &opcode.parameters, &mut intcode, &mut ip),
+            5 => jump_if(&opcode.parameters, &mut intcode, &mut ip),
+            6 => jump_unless(&opcode.parameters, &mut intcode, &mut ip),
+            7 => is_less_than(&opcode.parameters, &mut intcode, &mut ip),
+            8 => is_equal(&opcode.parameters, &mut intcode, &mut ip),
+            99 => return Ok(()),
             _ => bail!("invalid opcode: `{}`", opcode.code),
         }
     }
     Err(anyhow!("no end found"))
 }
 
-#[allow(dead_code)]
-fn bruteforce(intcode: &[i32], output: i32) -> Option<(i32, i32)> {
-    (0..99).cartesian_product(0..99).find(|&(noun, verb)| {
-        process_intcode(intcode, Some(noun), Some(verb)).unwrap()[0] == output
-    })
+#[test]
+fn test_equal_position_mode() {
+    let mut output = Vec::new();
+    process_intcode(
+        &[3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8],
+        &mut std::iter::once(8),
+        &mut output,
+    )
+    .unwrap();
+    assert_eq!(&output, &[1]);
+
+    let mut output = Vec::new();
+    process_intcode(
+        &[3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8],
+        &mut std::iter::once(9),
+        &mut output,
+    )
+    .unwrap();
+    assert_eq!(&output, &[0]);
 }
 
 #[test]
-fn test() {
-    assert_eq!(
-        *process_intcode(&[1, 0, 0, 0, 99], None, None)
-            .unwrap()
-            .first()
-            .unwrap(),
-        2
-    );
-    assert_eq!(
-        *process_intcode(&[2, 3, 0, 3, 99], None, None)
-            .unwrap()
-            .first()
-            .unwrap(),
-        2
-    );
-    assert_eq!(
-        *process_intcode(&[2, 4, 4, 5, 99, 0], None, None)
-            .unwrap()
-            .first()
-            .unwrap(),
-        2
-    );
-    assert_eq!(
-        *process_intcode(&[1, 1, 1, 4, 99, 5, 6, 0, 99], None, None)
-            .unwrap()
-            .first()
-            .unwrap(),
-        30
-    );
+fn test_less_than_position_mode() {
+    let mut output = Vec::new();
+    process_intcode(
+        &[3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8],
+        &mut std::iter::once(7),
+        &mut output,
+    )
+    .unwrap();
+    assert_eq!(&output, &[1]);
+
+    let mut output = Vec::new();
+    process_intcode(
+        &[3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8],
+        &mut std::iter::once(9),
+        &mut output,
+    )
+    .unwrap();
+    assert_eq!(&output, &[0]);
+}
+
+#[test]
+fn test_equal_immediate_mode() {
+    let mut output = Vec::new();
+    process_intcode(
+        &[3, 3, 1108, -1, 8, 3, 4, 3, 99],
+        &mut std::iter::once(8),
+        &mut output,
+    )
+    .unwrap();
+    assert_eq!(&output, &[1]);
+
+    let mut output = Vec::new();
+    process_intcode(
+        &[3, 3, 1108, -1, 8, 3, 4, 3, 99],
+        &mut std::iter::once(9),
+        &mut output,
+    )
+    .unwrap();
+    assert_eq!(&output, &[0]);
+}
+
+#[test]
+fn test_less_than_immediate_mode() {
+    let mut output = Vec::new();
+    process_intcode(
+        &[3, 3, 1107, -1, 8, 3, 4, 3, 99],
+        &mut std::iter::once(7),
+        &mut output,
+    )
+    .unwrap();
+    assert_eq!(&output, &[1]);
+
+    let mut output = Vec::new();
+    process_intcode(
+        &[3, 3, 1107, -1, 8, 3, 4, 3, 99],
+        &mut std::iter::once(9),
+        &mut output,
+    )
+    .unwrap();
+    assert_eq!(&output, &[0]);
+}
+
+#[test]
+fn test_jump_position_mode() {
+    let mut output = Vec::new();
+    process_intcode(
+        &[3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9],
+        &mut std::iter::once(0),
+        &mut output,
+    )
+    .unwrap();
+    assert_eq!(&output, &[0]);
+}
+
+#[test]
+fn test_jump_immediate_mode() {
+    let mut output = Vec::new();
+    process_intcode(
+        &[3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9],
+        &mut std::iter::once(0),
+        &mut output,
+    )
+    .unwrap();
+    assert_eq!(&output, &[0]);
+}
+
+#[test]
+fn test_jump_and_conditions() {
+    let intcode = vec![
+        3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0,
+        1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20,
+        1105, 1, 46, 98, 99,
+    ];
+    let mut output = Vec::new();
+    process_intcode(&intcode, &mut std::iter::once(7), &mut output).unwrap();
+    process_intcode(&intcode, &mut std::iter::once(8), &mut output).unwrap();
+    process_intcode(&intcode, &mut std::iter::once(9), &mut output).unwrap();
+    assert_eq!(&output, &[999, 1000, 1001]);
 }
